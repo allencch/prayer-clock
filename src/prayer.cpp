4c5fbe11cc43ca192caf32ef06d90343770ce45d
Allen <allencch@hotmail.com>
2025-05-25 21:11:02 +0800

    refactor: indent and static_cast

--- src/prayer.cpp
+++ src/prayer.cpp
@@ -20,82 +20,81 @@ using namespace std;
 #include "prayer.h"
 
 Prayer::Prayer() {
-    easter_f = false;
+  easter_f = false;
 }
 
 void Prayers::startListElement(void *userData, const char *name, const char **atts) {
-    PrayerList* pl = (PrayerList*)userData;
+  PrayerList* pl = static_cast<PrayerList*>(userData);
+  if(strcmp(name,"prayer") == 0) {
+    Prayer* pr = new Prayer;
+    pl->push_back(pr);
+  }
+  else if(pl->size()) {
+    pl->back()->element = name;
+  }
 
 
-    if(strcmp(name,"prayer") == 0) {
-        Prayer* pr = new Prayer;
-        pl->push_back(pr);
+  for(int i=0;atts[i];i+=2) {
+    if(strcmp(atts[i],"title") == 0) {
+      pl->back()->title = atts[i+1];
     }
-    else if(pl->size()) {
-        pl->back()->element = name;
+    else if(strcmp(atts[i],"easter") == 0) {
+      pl->back()->easter_f = true;
+      pl->back()->easter = atoi(atts[i+1]);
     }
-
-
-    for(int i=0;atts[i];i+=2) {
-        if(strcmp(atts[i],"title") == 0) {
-            pl->back()->title = atts[i+1];
-        }
-        else if(strcmp(atts[i],"easter") == 0) {
-            pl->back()->easter_f = true;
-            pl->back()->easter = atoi(atts[i+1]);
-        }
-    }
+  }
 }
 
 void Prayers::endListElement(void *userData, const char *name) {
-    PrayerList* pl = (PrayerList*)userData;
-    pl->back()->element = "";
+  PrayerList* pl = static_cast<PrayerList*>(userData);
+  pl->back()->element = "";
 }
 
 void Prayers::characterListHandler(void* userData,const XML_Char* s,int len) {
-    PrayerList* pl = (PrayerList*)userData;
-
-    if(pl->size() && pl->back()->element == "time") {
-        string t(s,len);
-        pl->back()->time.push_back(t);
-    }
-    else if(pl->size() && pl->back()->element =="text") {
-        string t(s,len);
-        pl->back()->text += t;
-    }
+  PrayerList* pl = static_cast<PrayerList*>(userData);
+  if(pl->size() && pl->back()->element == "time") {
+    string t(s,len);
+    pl->back()->time.push_back(t);
+  }
+  else if(pl->size() && pl->back()->element =="text") {
+    string t(s,len);
+    pl->back()->text += t;
+  }
 }
 
 
 Prayers::Prayers(const char* filename) {
-    char buf[8192];
-    XML_Parser parser = XML_ParserCreate(NULL);
-    int done;
+  char buf[8192];
+  XML_Parser parser = XML_ParserCreate(NULL);
+  int done;
 
-    XML_SetUserData(parser, &list);
-    XML_SetElementHandler(parser, startListElement, endListElement);
-    XML_SetCharacterDataHandler(parser,characterListHandler);
+  XML_SetUserData(parser, &list);
+  XML_SetElementHandler(parser, startListElement, endListElement);
+  XML_SetCharacterDataHandler(parser,characterListHandler);
 
-    FILE* file = fopen(filename,"rb");
-    if(!file)
-        fprintf(stderr,"Failed open file\n");
+  FILE* file = fopen(filename,"rb");
+  if(!file) {
+    fprintf(stderr,"Failed open file\n");
+    return;
+  }
 
-    do {
-        int len = (int)fread(buf, 1, sizeof(buf), file);
-        done = len < sizeof(buf);
-        if (XML_Parse(parser, buf, len, done) == XML_STATUS_ERROR) {
-            fprintf(stderr,
-                "%s at line %" XML_FMT_INT_MOD "u\n",
-                XML_ErrorString(XML_GetErrorCode(parser)),
-                XML_GetCurrentLineNumber(parser));
-            return;
-        }
-    } while (!done);
-    XML_ParserFree(parser);
-    fclose(file);
+  do {
+    int len = (int)fread(buf, 1, sizeof(buf), file);
+    done = len < sizeof(buf);
+    if (XML_Parse(parser, buf, len, done) == XML_STATUS_ERROR) {
+      fprintf(stderr,
+              "%s at line %" XML_FMT_INT_MOD "u\n",
+              XML_ErrorString(XML_GetErrorCode(parser)),
+              XML_GetCurrentLineNumber(parser));
+      return;
+    }
+  } while (!done);
+  XML_ParserFree(parser);
+  fclose(file);
 }
 
 Prayers::~Prayers() {
-    for(int i=0;i<list.size();i++) {
-        delete list[i];
-    }
+  for(int i=0;i<list.size();i++) {
+    delete list[i];
+  }
 }
